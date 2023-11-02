6244194ed84e821d257c50eac9c9c3f35b7ee5ed
Julian Montague <juliangm@stud.ntnu.no>
2023-11-02 13:14:46 +0100

    Remove Java module support
    This makes our project no longer a module.
    
    Modules [weren't added until Java 9 in 2017][1], so it's unlikely that
    JSDAI uses them. Plus, I've never properly learned how to use them.
    
    [1]: https://www.oracle.com/corporate/features/understanding-java-9-modules.html

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module no.aakp.digicat.json.to.cad.skyreal.vr {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens no.aakp.digicat.json.to.cad.skyreal.vr to javafx.fxml;
-    exports no.aakp.digicat.json.to.cad.skyreal.vr;
-}
